4bc009d61a95f7af423e488a128479d11a2d780b
viktorbeneitest <viktorbeneitest@users.noreply.github.com>
2018-02-20 16:23:41 +0100

    Update install_bitrise_cli.sh

--- install_bitrise_cli.sh
+++ install_bitrise_cli.sh
@@ -3,6 +3,6 @@ set -ex
 
 bitrise_cli_ver="1.13.0"
 
-sudo wget "https://github.com/bitrise-io/bitrise/releases/download/${bitrise_cli_ver}/bitrise-$(uname -s)-$(uname -m)" -O /usr/local/bin/bitrise
-sudo chmod +x /usr/local/bin/bitrise
+wget "https://github.com/bitrise-io/bitrise/releases/download/${bitrise_cli_ver}/bitrise-$(uname -s)-$(uname -m)" -O /usr/local/bin/bitrise
+chmod +x /usr/local/bin/bitrise
 bitrise setup
