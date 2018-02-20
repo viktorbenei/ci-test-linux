41184790f610dd690bb5cb115dd2a9cae181c5a2
viktorbeneitest <viktorbeneitest@users.noreply.github.com>
2018-02-20 16:14:41 +0100

    bitrise cli 1.13.0

--- install_bitrise_cli.sh
+++ install_bitrise_cli.sh
@@ -1,7 +1,6 @@
-#!/bin/bash
-set -e
-set -v
+#!/usr/bin/env bash
+set -ex
 
-curl -fL https://github.com/bitrise-io/bitrise/releases/download/1.2.4/bitrise-$(uname -s)-$(uname -m) > /usr/local/bin/bitrise
+curl -fL https://github.com/bitrise-io/bitrise/releases/download/1.13.0/bitrise-$(uname -s)-$(uname -m) > /usr/local/bin/bitrise
 chmod +x /usr/local/bin/bitrise
 bitrise setup --minimal
