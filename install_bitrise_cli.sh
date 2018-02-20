8239cb11237b27cf761dfd310c89fb283cc44939
viktorbeneitest <viktorbeneitest@users.noreply.github.com>
2018-02-20 16:19:25 +0100

    Update install_bitrise_cli.sh

--- install_bitrise_cli.sh
+++ install_bitrise_cli.sh
@@ -3,4 +3,4 @@ set -ex
 
 curl -fL https://github.com/bitrise-io/bitrise/releases/download/1.13.0/bitrise-$(uname -s)-$(uname -m) > /usr/local/bin/bitrise
 chmod +x /usr/local/bin/bitrise
-bitrise setup --minimal
+bitrise setup
